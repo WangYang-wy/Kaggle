931ab2055e2cf5159ea7e84bed301f33dab1d052
WangYang-wy <1070942929@qq.com>
2018-07-31 11:46:48 +0800

    重构。

---
+++ Titanic: Machine Learning from Disaster/code.py
@@ -0,0 +1,11 @@
+path = '~/.kaggle/competitions/titanic/'
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.style.use('fivethirtyeight')
+import warnings
+
+warnings.filterwarnings('ignore')
